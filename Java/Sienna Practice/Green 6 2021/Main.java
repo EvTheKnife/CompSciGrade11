499722fe5fa535a5e577d0ea131c1e67fac68b90
daKnife997 <evan.jorgensen@gmail.com>
2024-11-23 20:12:51 -0500

    20:13 11/23/24

--- Java/Sienna Practice/Green 6 2021/Main.java
+++ Java/Sienna Practice/Green 6 2021/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int userIn = sc.nextInt();
 
         
-
+				
         sc.close();
         
     }
---
+++ Python/Sienna Practice/Green 6 2022/default.py
@@ -0,0 +1,17 @@
+import math
+
+def clothing():
+	numRestrictions = int(input())
+
+	restrictions = []
+
+	for i in range(numRestrictions):
+		(color1, item1, color2, item2) = input().split()
+		restrictions.append([color1, item1, color2, item2])
+
+	if n = 0:
+		return 256
+	
+	else:
+		
+		
